aff768b5d1c9222d20d40c54791dbefc53168fba
cutillav <victorcutilla@hotmail.com>
2016-11-09 16:21:52 +0100

    Add validation

--- crimemap.py
+++ crimemap.py
@@ -3,6 +3,9 @@ from flask import render_template
 from flask import request
 import json
 import dbconfig
+import string
+
+categories = ['mugging','break-in']
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
@@ -11,21 +14,29 @@ else:
 app = Flask(__name__)
 DB = DBHelper()
 
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 @app.route("/")
 def home():
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes,categories=categories)
 
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
+    if category not in categories:
+        return home()
     date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
